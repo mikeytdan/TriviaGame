8e151704ded3d5137ec8c45863a281da366ab8b3
Michael Daniels <mikeytdan@gmail.com>
2018-05-19 10:39:21 -0600

    Finished the assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,27 +23,58 @@ var game = {
     infoObjects: [],
     timer: null,
     displayedInfo: null,
-    seconds: 30,
+    maxSeconds: 30,
+    seconds: this.maxSeconds,
     correct: 0,
     incorrect: 0,
 
     allInfoObjects: [
         {
-            question: "What is the answer to life?",
+            question: "What is the answer to life, the universe and everything?",
             answer: "42",
             answerImage: "https://media.giphy.com/media/BVlWY2vMZgLG8/giphy.gif",
             fakeAnswers: ["24", "13", "31"]
         },
         {
-            question: "What is not the answer to life?",
-            answer: "24",
-            answerImage: "https://media.giphy.com/media/BVlWY2vMZgLG8/giphy.gif",
-            fakeAnswers: ["42", "42", "42"]
+            question: "How many electrons does an Oxygen atom have?",
+            answer: "8",
+            answerImage: "https://media.giphy.com/media/lVbcNXfGBkG6Q/giphy.gif",
+            fakeAnswers: ["6", "9", "12"]
+        },
+        {
+            question: "What is Dihydrogen Monoxide",
+            answer: "Water",
+            answerImage: "https://media3.giphy.com/media/JXHhI4o9NCf8k/giphy.gif",
+            fakeAnswers: ["Cleaning Chemical", "Band", "Blimp"]
+        },
+        {
+            question: "How far is the Moon from the Earth?",
+            answer: "238,900 mi",
+            answerImage: "https://media.giphy.com/media/aN9GqoR7OD3nq/giphy.gif",
+            fakeAnswers: ["238,90 mi", "2,389,000 mi", "2,389 mi"]
+        },
+        {
+            question: "Which animal was introduced to Australia in 1859?",
+            answer: "Rabbits",
+            answerImage: "https://media.giphy.com/media/WiXMlla4ZFR8Q/giphy.gif",
+            fakeAnswers: ["Marsupial", "Dingo", "Kangaroo"]
+        },
+        {
+            question: "When was the United States founded?",
+            answer: "July 4, 1776",
+            answerImage: "https://media.giphy.com/media/sFMDqop2ku4M0/giphy.gif",
+            fakeAnswers: ["July 4, 1492", "July 4, 1912", "July 4, 1812"]
+        },
+        {
+            question: "What color is a smurf?",
+            answer: "Blue",
+            answerImage: "https://media.giphy.com/media/AxVvjLgE0EzJ4cF5C0/giphy.gif",
+            fakeAnswers: ["Purple", "Red", "Multicolor"]
         }
     ],
 
     clearTimer: function () {
-        this.seconds = 30;
+        this.seconds = this.maxSeconds;
         $("#seconds").text(this.seconds);
         if (this.timer) {
             clearTimeout(this.timer);
@@ -55,20 +86,19 @@ var game = {
         game.seconds--;
         $("#seconds").text(game.seconds);
         if (game.seconds <= 0) {
-
+            game.selectedAnswer(null);
         }
     },
 
     showNextQuestion: function () {
         $("#question-image").hide();
         if (this.infoObjects.length > 0) {
+            $("#countdown").show();
             $("#question-choices").show();
-            console.log("Test: " + this.infoObjects[0]);
             this.updateWithInfo(this.infoObjects.shift());
             this.timer = setInterval(this.countdown, 1000);
         } else {
             this.showEndGame()
-            // TODO: Show the end of game screen and add a reset button
         }
     },
 
@@ -91,29 +121,22 @@ var game = {
         $("#question-choices").show();
         $("#question-image").hide();
         this.displayedInfo = infoObject;
-        console.log(this.displayedInfo);
-        $("#question").text(infoObject.question);
+        $("#question").text("\"" + infoObject.question + "\"");
         var answers = infoObject.fakeAnswers.slice(); // Copy the fake answers array
         answers.push(infoObject.answer);
         answers = shuffle(answers);
         for (var i = 0; i < answers.length; i++) {
             $("#answer-" + (i + 1)).text(answers[i]);
         }
-        $("#answer-text").text("The correct answer is: " + infoObject.answer);
         $("#answer-image")[0].src = infoObject.answerImage;
-        console.log(this.displayedInfo);
-    },
-
-    showQuestionImage: function() {
-        // TODO: Move part of the setup logic here
     },
 
     newGame: function () {
+        this.seconds = this.maxSeconds;
         this.correct = 0;
         this.incorrect = 0;
         this.infoObjects = [];
         var shuffledInfoObjects = shuffle(this.allInfoObjects);
-        console.log("Test1: " + shuffledInfoObjects.length);
         var count = this.allInfoObjects.length > 5 ? 5 : this.allInfoObjects.length;
         for (var i = 0; i < count; i++) {
             // Add the first 5 shuffled info objects to `infoObjects`
@@ -121,33 +144,44 @@ var game = {
         }
     },
 
-    setup: function() {
+    selectedAnswer: function (answer) {
+        this.clearTimer();
+        $("#countdown").hide();
+        $("#question-choices").hide();
+        $("#question-image").show();
+        if (answer != null && answer == this.displayedInfo.answer) {
+            $("#result").html("<b>" + answer + "</b> is<br><h2>Correct!</font></h2>");
+            this.correct++;
+        } else {
+            if (answer == null) {
+                $("#result").html("<b>You ran out of time!</b>");
+            } else {
+                $("#result").html("<b>" + answer + "</b> is<br><h2>Wrong!</h2>");
+            }
+            this.incorrect++;
+        }
+        $("#answer-text").html("The correct answer is: <b>" + this.displayedInfo.answer + "</b>");
+        setTimeout(function () {
+            $("#result").html("");
+            $("#answer-text").text("");
+            this.showNextQuestion();
+        }.bind(this), 3000);
+    },
+
+    setup: function () {
         var self = this;
         $(".answer").on("click", function (event) {
-            $("#question-choices").hide();
-            $("#question-image").show();
-            self.clearTimer();
-            setTimeout(function () {
-                self.showNextQuestion();
-            }.bind(self), 1000);
-            console.log(event);
             var clickedText = $(this).text();
-            if (clickedText == self.displayedInfo.answer) {
-                $("#result").html("<b>" + clickedText + "</b> is <b>correct!</b>");
-                self.correct++;
-            } else {
-                $("#result").html("<b>" + clickedText + "</b> is <b>wrong!</b>");
-                self.incorrect++;
-            }
+            self.selectedAnswer(clickedText);
         });
     }
 
 };
 
 window.onload = function (event) {
-    console.log("window.onload");
     game.setup();
     $("#end-game").hide();
+    $("#countdown").hide();
     $("#question-choices").hide();
     $("#question-image").hide();
     $("#start-game").on("click", function (event) {
